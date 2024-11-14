f0bfc4ae06181f6aa3c02d7ea763e6976228bd7d
fljs825 <lastbartican825@gmail.com>
2024-11-14 15:30:37 +0900

    test

--- timecalib_sine/sineana.sh
+++ timecalib_sine/sineana.sh
@@ -1,1 +1,9 @@
-../v2.1/sineana.sh
+#!/bin/bash
+
+root -l -b -q "timingtree.cc"
+wait
+hadd -f timingtree.root timingtree_ch*
+#rm timingtree_ch*
+root -l -b -q "./process.cc"
+echo "All processes are done"
+
